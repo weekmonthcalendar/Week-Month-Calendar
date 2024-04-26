9cd02255db3287e9d61f31047eacc610277bd8a0
weekmonthcalendar <168192126+weekmonthcalendar@users.noreply.github.com>
2024-04-26 17:59:28 +0530

    Update index.js
    error

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
         <Header title="Printable Calendar" />
         <p className="description">
           Plan your endeavours far enough in advance to prevent wasting time and valuable resources. Always save a <a href="https://weekmonthcalendar.com/">Yearly Printable Calendar</a> January to your computer to note key occasions like holidays, events, birthdays, etc. so that you can finish your work ahead of schedule without interfering with your regular daily tasks.
-        <br><br>
+        
     One of the best tools for planning and finishing tasks is the <a href="https://weekmonthcalendar.com/february-blank-calendar/">February 2025 Blank Calendar</a>. If you want to succeed in your daily tasks, using a calendar is a great idea because it will help you remember to do things on time.
-   <br><br>
+   
     The PDF of the <a href="https://weekmonthcalendar.com/march-april-2025-calendar/">March April 2025 Calendar</a> PDF is among the best resources for organising and completing tasks. Using a calendar is a wonderful idea if you want to succeed in your daily duties because it will help you remember to complete things on time.
     </p>
       </main>
